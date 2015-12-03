3419cf76c23af75f352dd70eef293ad39b18640f
Izabella Konstanciak <i.konstanciak@gmail.com>
2015-12-03 15:44:34 +0100

    Add 'expose(:studentent)' to the controller to display students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+  expose(:students) #Iza
   expose(:student, attributes: :student_params)
   expose(:student_subject_items) { student.subject_items }
 
