156e72665a06ce2c2a98c263229610d44b10479a
Izabella Konstanciak <i.konstanciak@gmail.com>
2015-12-03 02:13:55 +0100

    Create new routes to display subpages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'visitors#index'
+  
   resources :students do
     get :subjects
   end
+  
+  #get "/reports/subjects" =>  "/reports/subjects"
+  resources :report_subjects
+  #get "/reports/subjects" => :report_subjects
+  
+  resources :teachers do
+    get :subjects
+  end
+  resources :users
+  resources :user_sessions, only: [:new, :create]
+  #resources :user_registration, only: [:new, :create]
+  devise_for :controllers => { registrations: 'registrations' }
+  get 'registrations/signup' => 'devise/registrations#new', :as => :new_user_registration
 end
