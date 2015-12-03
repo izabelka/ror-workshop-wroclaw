03c4edc7e0a5af73c73dab1d73d6a481dfb15d14
Izabella Konstanciak <i.konstanciak@gmail.com>
2015-12-03 15:46:31 +0100

    Generate registrations controller, add devise_for in routes, change the user model to work with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,31 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'visitors#index'
   
-  resources :students do
-    get :subjects
-  end
+  #devise_for :users, :controllers => { registrations: 'registrations' }
+  #resources :users
   
-  #get "/reports/subjects" =>  "/reports/subjects"
-  resources :report_subjects
-  #get "/reports/subjects" => :report_subjects
+   #devise_scope :user do
+     #get "/signup" => "registrations#new", :as => :new_user_registration
+     #get "/users" => "devise/registrations#create", :as => :user_registration
+     #end
+   
+   #user_registration
+   root to: 'visitors#index'
   
-  resources :teachers do
-    get :subjects
-  end
-  resources :users
-  resources :user_sessions, only: [:new, :create]
-  #resources :user_registration, only: [:new, :create]
-  devise_for :controllers => { registrations: 'registrations' }
-  get 'registrations/signup' => 'devise/registrations#new', :as => :new_user_registration
+   resources :students do
+     get :subjects
+   end
+  
+   #get "/reports/subjects" =>  "/reports/subjects"
+   resources :report_subjects
+   #get "/reports/subjects" => :report_subjects
+  
+   resources :teachers do
+     get :subjects
+   end
+   #resources :users
+   #resources :user_sessions, only: [:new, :create]
+   #resources :user_registration #, only: [:new, :create]
+   #get 'registrations/signup' => 'devise/registrations#new', :as => :new_user_registration
+
 end
