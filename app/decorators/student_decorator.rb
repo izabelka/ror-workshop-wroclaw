51b6d795beb84085589488527aa61293a7f1cfd0
Izabella Konstanciak <i.konstanciak@gmail.com>
2015-12-05 17:38:05 +0100

    Add missing methods to decorators

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,8 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    notes = subject_item.subject_item_notes
+    return '0.00' if notes.blank?
+    format('%.2f', notes.sum(:value).to_f / notes.length)
   end
 end
