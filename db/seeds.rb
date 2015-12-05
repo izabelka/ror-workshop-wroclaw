521f061e99e53b402d6a54608b16a67c4c16bcb4
Izabella Konstanciak <i.konstanciak@gmail.com>
2015-12-05 17:41:32 +0100

    Prepare seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
 puts "Seeds: start"
 TEACHER_TITLES = %w(Dr. Prof. TA)
+#admin
 User.create!(email: 'admin@admin.com',password: 'adminadmin')
+#User.create!(email: 'dupa22@dupa22.com',password: 'dupadupa222')
 
-30.times do
+#40
+3.times do
   Teacher.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
@@ -11,14 +14,14 @@ User.create!(email: 'admin@admin.com',password: 'adminadmin')
 end
 
 teachers = Teacher.all
-20.times do
+5.times do
   SubjectItem.create!(
     title: Faker::Lorem.sentence,
     teacher: teachers.sample
   )
 end
 
-40.times do
+25.times do
   Student.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name
