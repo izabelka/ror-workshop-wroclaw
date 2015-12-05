b52d4528af1e42b73e144cdca06e2aa43768cb1f
Izabella Konstanciak <i.konstanciak@gmail.com>
2015-12-05 17:41:11 +0100

    Configure routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'visitors#index'
   
-  #devise_for :users, :controllers => { registrations: 'registrations' }
-  #resources :users
-  
-   #devise_scope :user do
-     #get "/signup" => "registrations#new", :as => :new_user_registration
-     #get "/users" => "devise/registrations#create", :as => :user_registration
-     #end
-   
-   #user_registration
-   root to: 'visitors#index'
-  
-   resources :students do
+  resources :students do
      get :subjects
    end
-  
-   #get "/reports/subjects" =>  "/reports/subjects"
-   resources :report_subjects
-   #get "/reports/subjects" => :report_subjects
   
    resources :teachers do
      get :subjects
    end
-   #resources :users
-   #resources :user_sessions, only: [:new, :create]
-   #resources :user_registration #, only: [:new, :create]
-   #get 'registrations/signup' => 'devise/registrations#new', :as => :new_user_registration
-
+     
+   get "/reports/subjects" => "reports#subjects", as: :report_subjects # Iza - works fine
 end
